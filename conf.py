42b960dfcf395fbccc111aee526d592a1f388fd5
Hessam Bagherinezhad <hbagherinezhad@gmail.com>
2014-11-09 17:42:03 -0800

    Merge conflict

--- conf.py
+++ conf.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class DATASET:
     class TRAINING:
         USERS = 480189
@@ -11,18 +10,6 @@ class FILES:
     UID = 'data/uid.npy'
 
 class MATRIX_FAC:
-    LAMBDA = 0.2
-    ALPHA = 0.002
-    NSTEP = 5
-=======
-'''
-Created on Nov 6, 2014
-
-@author: areza_000
-'''
-
-class MATRIX_FAC:
     LAMBDA = 0.02
     ALPHA = 0.0002
-    NSTEP = 5000 
->>>>>>> e58b2ccbfa95ff810de84ef5cfc1b50702347e1b
+    NSTEP = 5000
